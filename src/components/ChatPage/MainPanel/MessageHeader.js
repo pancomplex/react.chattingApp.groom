a0d13e5440cf1402d766946061916aa1d3fe9cc7
pancomplex <renaissance21c@gmail.com>
2021-08-01 17:06:40 +0900

    [8/1] working on MessageHeader

--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,7 +1,77 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Image from "react-bootstrap/Image";
+import Accordion from "react-bootstrap/Accordion";
+import { FaLock } from "react-icons/fa";
+import { MdFavorite } from "react-icons/md";
+import { AiOutlineSearch } from "react-icons/ai";
 
 function MessageHeader() {
-  return <div>MessageHeader</div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "170px",
+        border: " 0.2rem solid #ececec",
+        borderRadius: "4px",
+        padding: "1rem",
+        marginBottom: "1rem",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col>
+            <h2>
+              <FaLock />
+              ChatRoomName
+              <MdFavorite />
+            </h2>
+          </Col>
+          <Col>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">
+                <AiOutlineSearch />
+              </InputGroup.Text>
+              <FormControl
+                placeholder="Search Messages"
+                aria-label="Search"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <p>
+              <Image src="" /> username
+            </p>
+          </div>
+        </Row>
+        <Row>
+          <Col>
+            <Accordion defaultActiveKey="0">
+              {/* <Accordion.Item>
+                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                <Accordion.Body>Lorem ipsum dolor sit amet,</Accordion.Body>
+              </Accordion.Item> */}
+            </Accordion>
+          </Col>
+          <Col>
+            <Accordion>
+              {/* <Accordion.Item eventKey="1">
+                <Accordion.Header>Accordion Item #2</Accordion.Header>
+                <Accordion.Body>Lorem ipsum dolor sit amet, consectetur</Accordion.Body>
+              </Accordion.Item> */}
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default MessageHeader;
--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,7 +1,173 @@
-import React from "react";
+import React, { Component } from "react";
+import firebase from "../../../firebase";
+import { connect } from "react-redux";
+import { setCurrentChatRoom } from "../../../redux/actions/chatRoom_action";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { FaRegSmileWink, FaPlus } from "react-icons/fa";
 
-function ChatRooms() {
-  return <div>ChatRooms</div>;
+export class ChatRooms extends Component {
+  state = {
+    show: false,
+    name: "",
+    description: "",
+    chatRoomsRef: firebase.database().ref("chatRooms"),
+    chatRooms: [],
+    firstLoad: true,
+    activeChatRoomId: "",
+  };
+
+  componentDidMount() {
+    this.AddChatRoomsListeners();
+  }
+
+  componentWillUnmount() {
+    this.state.chatRooms.off();
+  }
+
+  setFirstChatRoom = () => {
+    const firstChatRoom = this.state.chatRooms[0];
+    if (this.state.firstLoad && this.state.chatRooms.length > 0) {
+      this.props.dispatch(setCurrentChatRoom(firstChatRoom));
+      this.setState({ activeChatRoomId: firstChatRoom.id });
+    }
+    this.setState({ firstLoad: false });
+  };
+
+  AddChatRoomsListeners = () => {
+    let chatRoomsArray = [];
+
+    this.state.chatRoomsRef.on("child_added", (DataSnapshot) => {
+      chatRoomsArray.push(DataSnapshot.val());
+      this.setState({ chatRooms: chatRoomsArray }, () => this.setFirstChatRoom());
+    });
+  };
+
+  handleClose = () => this.setState({ show: false });
+  handleShow = () => this.setState({ show: true });
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, description } = this.state;
+
+    if (this.isFormValid(name, description)) {
+      this.addChatRoom();
+    }
+  };
+  isFormValid = (name, description) => name && description;
+
+  changeChatRoom = (room) => {
+    this.props.dispatch(setCurrentChatRoom(room));
+    this.setState({ activeChatRoomId: room.id });
+  };
+
+  renderChatRooms = (chatRooms) =>
+    chatRooms.length > 0 &&
+    chatRooms.map((room) => (
+      <li
+        key={room.id}
+        onClick={() => this.changeChatRoom(room)}
+        style={{
+          backgroundColor: room.id === this.state.activeChatRoomId && "#ffffff45",
+          cursor: "pointer",
+        }}
+      >
+        # {room.name}
+      </li>
+    ));
+
+  addChatRoom = async () => {
+    const key = this.state.chatRoomsRef.push().key;
+    const { name, description } = this.state;
+    const { user } = this.props;
+    const newChatRoom = {
+      id: key,
+      name: name,
+      description: description,
+      createdBy: {
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+
+    try {
+      await this.state.chatRoomsRef.child(key).update(newChatRoom);
+      this.setState({
+        name: "",
+        description: "",
+        show: false,
+      });
+    } catch (error) {}
+  };
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <FaRegSmileWink style={{ marginRight: 3 }} />
+          CHAT ROOMS {""} (1)
+          <FaPlus
+            style={{
+              position: "absolute",
+              right: 0,
+              cursor: "pointer",
+            }}
+            onClick={this.handleShow}
+          />
+        </div>
+
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {this.renderChatRooms(this.state.chatRooms)}
+        </ul>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a chat room</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>방 이름</Form.Label>
+                <Form.Control
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                  type="text"
+                  placeholder="Enter a chat room name"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>방 설명</Form.Label>
+                <Form.Control
+                  onChange={(e) => this.setState({ description: e.target.value })}
+                  type="text"
+                  placeholder="Enter a chat room description"
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleSubmit}>
+              Create
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default ChatRooms;
+const mapStateToProps = (state) => {
+  return { user: state.user.currentUser };
+};
+
+export default connect(mapStateToProps)(ChatRooms);
